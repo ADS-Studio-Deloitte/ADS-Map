34f8ef7ff749b47491d7a8ca9f52a6dba1431532
QXZ2PJG <dawid.kondziela@partner.bmwgroup.com>
2023-10-25 18:05:59 +0200

    Add color picker

--- src/app/components/add-marker-dialog/add-marker-dialog.component.ts
+++ src/app/components/add-marker-dialog/add-marker-dialog.component.ts
@@ -1,7 +1,9 @@
 import {Component} from '@angular/core';
 import {MatDialogRef} from '@angular/material/dialog';
 import {MarkerType} from '../../shared/models/marker.model';
-import {FormBuilder, FormGroup} from '@angular/forms';
+import {AbstractControl, FormBuilder, FormControl, FormGroup} from '@angular/forms';
+import {ThemePalette} from '@angular/material/core';
+import {Color} from '@angular-material-components/color-picker';
 
 @Component({
   selector: 'app-add-marker-dialog',
@@ -9,11 +11,15 @@ import {FormBuilder, FormGroup} from '@angular/forms';
   styleUrls: ['./add-marker-dialog.component.css']
 })
 export class AddMarkerDialogComponent {
-  markerInputFormGroup: FormGroup = this.fb.group<{who: string, description: string, type: MarkerType}>({
+  markerInputFormGroup: FormGroup = this.fb.group<{who: string, description: string, color: Color}>({
     who: '',
     description: '',
-    type: MarkerType.VACATION
+    color: new Color(0, 255, 255)
   });
+
+  disabled = false;
+  touchUi = false;
+  color: ThemePalette
 
   constructor(
     public dialogRef: MatDialogRef<AddMarkerDialogComponent>,
